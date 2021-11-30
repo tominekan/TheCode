1cd1173616d42323af60f842db288f92acdd17bf
a2k6 <adrianpanezic@comcast.net>
2021-11-30 08:50:34 -0800

    Update secretpage.js

--- secretpage.js
+++ secretpage.js
@@ -1,19 +1,5 @@
-var running = true, list = ["H", "E", "L"], glitch;
-
-function delay(x){
-    return new Promise(function(resolve){
-        setTimeout(resolve, x);
-    });
-}
-
-
-
 
 
 function glitch(){
-    glitch = document.getElementById("glitch-title");
-    while (running){
-        
-    }
-}    
     
+}
