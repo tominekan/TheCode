c78efdb07458bf82bf1b77075d916a3f9d1cb694
a2k6 <adrianpanezic@comcast.net>
2021-11-29 23:23:44 -0800

    Tried(and failed) to animate the page title
    Co-Authored-By: Tomi Adenekan <70237683+tominekan@users.noreply.github.com>

--- secretpage.js
+++ secretpage.js
@@ -1,5 +1,21 @@
+var running = true, list = ["H", "E", "L"]
+
+/*function delay(x){
+    return new Promise(function(resolve)){
+        setTimeout(resolve, x)
+    }
+}*/
+
+
+
 
 
 function animation(){
-    document.getElementById("glitch-title");
-}
+    alert(document.title = "Haha I changed it suckerrrrrrrrrrrrrrrrrrrrrrrr");
+    /*while (running) {
+        for(var i = 0; i < list.length; i++){
+            setTimeout(document.title = list[i], 250);
+        }
+    */}    
+    
+//Y0u sH01dN"T Bee Sea1!Ng Th!s
