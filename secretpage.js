6b984d060740a8ec3c3c80917b4196757c5415bc
Tomi Adenekan <tominekan@outlook.com>
2021-12-01 10:14:55 -0800

    Fixed startpage

--- secretpage.js
+++ secretpage.js
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
+var title = "What the fuck a nigga tellin me";
+var speed = 300;
+var i = 0;
+document.title = ""
+// The spaces aren't working for some reason
+function animateTitle() {
+    if (i < title.length) {
+        document.title += title.charAt(i);
+        i++;
+        setTimeout(animateTitle, speed);
+    }
+=======
 
 
 function glitch(){
     
+>>>>>>> 1cd1173616d42323af60f842db288f92acdd17bf
 }
