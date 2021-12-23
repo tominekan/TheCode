7e4c128035aa1fb5a9004f960dff68d434702fe4
a2k6 <adrianpanezic@comcast.net>
2021-12-22 20:42:56 -0800

    Gave up on animating the glitching text in CSS and started attempting to animate it in java

--- secretpage.js
+++ secretpage.js
@@ -1,15 +1,11 @@
-var array = ["Y0uSH01n;tBeeSea1!ngTh!s", "*insert coded message*", "OOOOOOOGGGGGGGGGAAAAAAAAAAABBBBBBBBBOOOOOOOOOOGGGGGGGGGGGAAAAAAAAA"];
-var speed = 2500;
-var bool = true;
-document.title = "";
+var test = document.getElementByIdbyID("glitch");
 
-function animateTitle() {
-    var i = 0;
-    if (i < array.length) {
-        document.title = array[i];
-        setTimeout(animateTitle, speed);
-        i++;
-    }
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve, n);
+    });
 }
 
-function 
+async function glitch (){
+    test.color = 255;
+}
